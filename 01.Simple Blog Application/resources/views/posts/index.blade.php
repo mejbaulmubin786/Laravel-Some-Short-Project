0cba3915caba825a3377f0775e7d65e2feefce97
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-12-17 07:40:55 +0600

    Add show

--- 01.Simple Blog Application/resources/views/posts/index.blade.php
+++ 01.Simple Blog Application/resources/views/posts/index.blade.php
@@ -2,23 +2,26 @@
 
 @section('content')
     <h1>All Posts</h1>
-    <a href="{{ route('posts.create') }}" class="btn btn-primary">Create New Post</a>
-    <table class="table table-bordered mt-3">
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+    <a href="{{ route('posts.create') }}" class="btn btn-primary mb-3">Create New Post</a>
+
+    <table class="table table-bordered">
         <thead>
             <tr>
-                <th>#</th>
                 <th>Title</th>
                 <th>Actions</th>
             </tr>
         </thead>
         <tbody>
-            @foreach ($posts as $post)
+            @foreach($posts as $post)
                 <tr>
-                    <td>{{ $post->id }}</td>
                     <td>{{ $post->title }}</td>
                     <td>
+                        <a href="{{ route('posts.show', $post->id) }}" class="btn btn-info">Show</a>
                         <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning">Edit</a>
-                        <form action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display:inline-block;">
+                        <form action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display:inline;">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-danger">Delete</button>
@@ -28,5 +31,6 @@
             @endforeach
         </tbody>
     </table>
+
     {{ $posts->links() }}
 @endsection
