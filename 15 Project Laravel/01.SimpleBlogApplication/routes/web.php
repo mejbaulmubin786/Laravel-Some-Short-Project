cdd0ce6132acd4d974d321665ce0442e41b1fd4e
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-12-30 02:34:19 +0600

    Update Admin Dashboard

--- 15 Project Laravel/01.SimpleBlogApplication/routes/web.php
+++ 15 Project Laravel/01.SimpleBlogApplication/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+route::get('/home', [AdminController::class, 'index']);
