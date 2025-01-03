1eeaf707b223636afa99cac070d1ef959790ee13
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2025-01-04 02:15:19 +0600

    to to app

--- 15 Project Laravel/02.to-do-app/routes/web.php
+++ 15 Project Laravel/02.to-do-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,8 +13,17 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/welcome', function () {
+    // ডাটাবেজ থেকে সমস্ত টাস্ক ফেচ করা
+    $tasks = Task::all();
+
+    // ভিউতে $tasks ভ্যারিয়েবল পাঠানো
+    return view('index', compact('tasks'));
 });
+
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
